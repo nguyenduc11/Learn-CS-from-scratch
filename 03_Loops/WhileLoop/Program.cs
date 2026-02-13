038d167b1f14704d3389090f078fb414a7e9596d
nguyenduc <nguyenduc11@gmail.com>
2026-02-13 10:04:50 +0700

    Add loop examples (while, for, foreach) and array collection demonstrations

---
+++ 03_Loops/WhileLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)   
+        {
+            // While Loop
+
+            // int i = 0;
+            // while (i<5){
+            //     System.Console.WriteLine(i);
+            //     i++;
+            // }
+
+            // For Loop
+            for (int i = 0; i < 10; i++){
+                System.Console.WriteLine(i);
+                for (int j = 0; j < 10; j++){
+                    System.Console.WriteLine($"i*j equal {i*j}");
+                }
+            }
+
+            // Array Loop
+            string[] cars = {"Volvo","BMW","Ford","Mazda"};
+            foreach (string i in cars){
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 07_Collections/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Collections;
+public class Program{
+    static void Main(string[] args){
+        System.Console.WriteLine("let's learn C# collections");
+        string[] cars = {"volvo","bmw","ford","mazda"};
+        int[] myNum = {1,2,3,4,5,6,7};
+        System.Console.WriteLine(cars);
+        System.Console.WriteLine(myNum);
+
+        for (int i=0; i < cars.Length; i++){
+            System.Console.WriteLine(cars[i]);
+        }
+
+        foreach (int num in myNum){
+            System.Console.WriteLine(num);
+        }
+    }
+}
