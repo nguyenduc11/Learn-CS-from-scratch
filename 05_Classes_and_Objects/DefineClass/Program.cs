e5501be91334d379ade2af1ea0e2c24cc6059647
nguyenduc <nguyenduc11@gmail.com>
2026-02-13 11:31:16 +0700

    Add method examples (AverageValue, SayHi) and demonstrate LINQ operations (Max, Sort) on arrays

---
+++ 05_Classes_and_Objects/DefineClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DefineClass
+{
+    public class Program
+    {
+        // Define a Player class
+        public class Player{
+            // class field
+            string name;
+            int age;
+
+            // constructor
+
+            public Player(string name, int age){
+                this.name = name;
+                this.age = age;
+            }
+
+            // class method
+            public void Introduce(string country){
+                System.Console.WriteLine($"Hi, I am {name} from {country}. I am {age} years old");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("let's learn C# Classes and Objects");
+
+            // instantiate object
+            Player messi = new Player("Messi",35);
+            messi.Introduce("Argentina");
+
+            // other object from Player class
+            Player suarez = new Player("Suarez",37);
+            suarez.Introduce("Uruguay");
+        }
+    };
+}
+
+
--- 07_Collections/Arrays/Program.cs
+++ 07_Collections/Arrays/Program.cs
@@ -1,19 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 namespace Collections;
-public class Program{
-    static void Main(string[] args){
+
+public class Program
+{
+
+    // C# method
+    static double AverageValue(double num1, double num2)
+    {
+        double value = (num1 + num2) / 2;
+        return value;
+    }
+
+    static void SayHi()
+    {
+        System.Console.WriteLine("HI, method executed");
+    }
+
+
+    static void Main(string[] args)
+    {
         System.Console.WriteLine("let's learn C# collections");
-        string[] cars = {"volvo","bmw","ford","mazda"};
-        int[] myNum = {1,2,3,4,5,6,7};
-        System.Console.WriteLine(cars);
-        System.Console.WriteLine(myNum);
+        // string[] cars = {"volvo","bmw","ford","mazda"};
+        // Array.Sort(cars);
+        // int[] myNum = {1,2,3,4,5,6,700};
+        // System.Console.WriteLine(myNum.Max());
+        // System.Console.WriteLine(cars);
+        // System.Console.WriteLine(myNum);
 
-        for (int i=0; i < cars.Length; i++){
-            System.Console.WriteLine(cars[i]);
-        }
+        // for (int i=0; i < cars.Length; i++){
+        //     System.Console.WriteLine(cars[i]);
+        // }
 
-        foreach (int num in myNum){
-            System.Console.WriteLine(num);
-        }
+        // foreach (int num in myNum){
+        //     System.Console.WriteLine(num);
+        // }
+
+        System.Console.WriteLine(AverageValue(100, 300));
+        SayHi();
     }
 }
