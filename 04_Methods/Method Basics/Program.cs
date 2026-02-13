5eaca8681e35b9935f968a3bd6d37122a1f5a313
nguyenduc <nguyenduc11@gmail.com>
2026-02-13 12:03:23 +0700

    Fix CalAverage calculation and add Car class with color property demonstration

--- 04_Methods/Method Basics/Program.cs
+++ 04_Methods/Method Basics/Program.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 
-namespace MethodBasics{
-    public class Program{
+namespace MethodBasics
+{
+    public class Program
+    {
         // method that not return anything
-        public static void SayHi(string country){
+        public static void SayHi(string country)
+        {
             System.Console.WriteLine($"Hi, I am from {country}");
         }
 
         // method return double and accept integer arguments
-        public static double CalAverage(int num1, int num2){
-            double calValue = (num1 +num2 )/2;
+        public static double CalAverage(int num1, int num2)
+        {
+            double calValue = (double)(num1 + num2) / 2;
             return calValue;
         }
 
-        static void Main(string[] args){
+        static void Main(string[] args)
+        {
             System.Console.WriteLine("Let's learn C# methods");
 
-        SayHi("Vietnam");
-        System.Console.WriteLine(CalAverage(5,10));
-
+            SayHi("Vietnam");
+            double userValue = CalAverage(5, 100);
+            System.Console.WriteLine(userValue);
         }
     }
 }
--- 05_Classes_and_Objects/DefineClass/Program.cs
+++ 05_Classes_and_Objects/DefineClass/Program.cs
@@ -2,39 +2,66 @@
 
 namespace DefineClass
 {
-    public class Program
+    // Define a Player class
+    public class Player
     {
-        // Define a Player class
-        public class Player{
-            // class field
-            string name;
-            int age;
+        // class field
+        string name;
+        int age;
 
-            // constructor
+        // constructor
 
-            public Player(string name, int age){
-                this.name = name;
-                this.age = age;
-            }
-
-            // class method
-            public void Introduce(string country){
-                System.Console.WriteLine($"Hi, I am {name} from {country}. I am {age} years old");
-            }
-
+        public Player(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
         }
 
+        // class method
+        public void Introduce(string country)
+        {
+            System.Console.WriteLine($"Hi, I am {name} from {country}. I am {age} years old");
+        }
+
+    }
+
+
+    // Defince a Car class
+    public class Car
+    {
+        string color;
+
+        public Car(string color)
+        {
+            this.color = color;
+        }
+
+        public void ShowColor()
+        {
+            System.Console.WriteLine($"This car has {color} colour");
+        }
+    }
+
+    public class Program
+    {
         static void Main(string[] args)
         {
             System.Console.WriteLine("let's learn C# Classes and Objects");
 
             // instantiate object
-            Player messi = new Player("Messi",35);
+            Player messi = new Player("Messi", 35);
             messi.Introduce("Argentina");
 
             // other object from Player class
-            Player suarez = new Player("Suarez",37);
+            Player suarez = new Player("Suarez", 37);
             suarez.Introduce("Uruguay");
+
+
+            Car car01 = new Car("red");
+            car01.ShowColor();
+
+            Car car02 = new Car("green");
+            car02.ShowColor();
         }
     };
 }
