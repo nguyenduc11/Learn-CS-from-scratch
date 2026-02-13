14f3c3a2df4722438ff32b8b28a25daff0b2bc33
nguyenduc <nguyenduc11@gmail.com>
2026-02-13 11:46:24 +0700

    Add Method Basics project demonstrating void and return methods with parameters

---
+++ 04_Methods/Method Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodBasics{
+    public class Program{
+        // method that not return anything
+        public static void SayHi(string country){
+            System.Console.WriteLine($"Hi, I am from {country}");
+        }
+
+        // method return double and accept integer arguments
+        public static double CalAverage(int num1, int num2){
+            double calValue = (num1 +num2 )/2;
+            return calValue;
+        }
+
+        static void Main(string[] args){
+            System.Console.WriteLine("Let's learn C# methods");
+
+        SayHi("Vietnam");
+        System.Console.WriteLine(CalAverage(5,10));
+
+        }
+    }
+}
